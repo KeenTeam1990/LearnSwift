31468f3935695087485cc2a2c506cb1842f0d5ea
KeenTeam <812144991@qq.com>
2017-10-09 16:16:12 +0800

    Learn-Swift

--- LearnSwift/TwoAreaViewController.swift
+++ LearnSwift/TwoAreaViewController.swift
@@ -9,26 +9,81 @@
 import UIKit
 import MapKit
 
-class TwoAreaViewController: UIViewController {
+class TwoAreaViewController: UIViewController , MKMapViewDelegate{
 
     var areaName : String?
+    var mapView  : MKMapView?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "地图"
         setMapKitView()
+        setMapKit()
+        
     }
 
     func setMapKitView() {
         
-        let mapView = MKMapView(frame: CGRect(x: 0, y: 0, width: KTSCREENWIDTH, height: KTSCREENHIGHT))
-        
-        view.addSubview(mapView)
-        
-        
+        mapView = MKMapView(frame: CGRect(x: 0, y: 0, width: KTSCREENWIDTH, height: KTSCREENHIGHT))
+        mapView?.delegate = self
+        view.addSubview(mapView!)
+        if #available(iOS 9.0, *) {
+            mapView?.showsScale = true
+            mapView?.showsCompass = true
+            mapView?.showsTraffic = true
+            mapView?.showsUserLocation = true
+        }
         
     }
     
+    func setMapKit() {
+        
+        let coder = CLGeocoder()
+        coder.geocodeAddressString(areaName!) { (ps, error) in
+            guard let ps = ps else{
+            print(error ?? "未知错误")
+            return
+            }
+            
+            let place = ps.first
+            let annotation = MKPointAnnotation()
+            annotation.title = self.areaName
+            annotation.subtitle = self.areaName
+            
+            if let loc = place?.location{
+            
+                annotation.coordinate = loc.coordinate
+                self.mapView?.showAnnotations([annotation], animated: true)
+                self.mapView?.selectAnnotation(annotation, animated: true)
+            
+            }
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        if annotation is MKUserLocation{
+        return nil
+        }
+        
+        let id = "myAnontationId"
+        var av = mapView.dequeueReusableAnnotationView(withIdentifier: id
+        ) as? MKPinAnnotationView
+        if av == nil {
+            av = MKPinAnnotationView(annotation: annotation, reuseIdentifier: id)
+            av?.canShowCallout = true
+        }
+        
+        let leftIconView = UIImageView(frame: CGRect(x: 0, y: 0, width: 53, height: 53))
+        leftIconView.image = UIImage(named:"appicon")
+        av?.leftCalloutAccessoryView = leftIconView
+        if #available(iOS 9.0, *) {
+            av?.pinTintColor = UIColor.green
+        }
+        
+     return av
+    }
     
     
     override func didReceiveMemoryWarning() {
--- LearnSwift/TwoViewController.swift
+++ LearnSwift/TwoViewController.swift
@@ -24,7 +24,7 @@ class TwoViewController: UIViewController, UITableViewDelegate, UITableViewDataS
     
     func setDataSource(){
     
-        let dataAr = ["北海","云居寺"," 中央电视塔","十三陵","天安门","雍和宫","玉渊潭","八达岭","颐和园","什刹海","龙庆峡","潭柘寺","九龙游乐园","幽谷神潭","康西草原","京都第一瀑","锡林浩特景区","拉法山国家公园","东平国家森林公园","连州地下河","虎跳峡","九寨沟","珠穆朗玛峰","嘉峪关关城"]
+        let dataAr = ["北海","云居寺"," 中央电视塔","十三陵","天安门","雍和宫","玉渊潭","八达岭","颐和园","什刹海","龙庆峡","潭柘寺","九龙游乐园","幽谷神潭","康西草原","京都第一瀑","锡林浩特景区","拉法山国家公园","东平国家森林公园","连州地下河","虎跳峡","九寨沟","珠穆朗玛峰","嘉峪关关城","单县一中","同叶大厦"]
         
        dataArr.addObjects(from: dataAr)
 
@@ -44,6 +44,7 @@ class TwoViewController: UIViewController, UITableViewDelegate, UITableViewDataS
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
         return dataArr.count
     }
     
@@ -57,17 +58,17 @@ class TwoViewController: UIViewController, UITableViewDelegate, UITableViewDataS
     }
     
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        
           return 0.0000001
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         let twoAreaVC = TwoAreaViewController()
-       
         twoAreaVC.areaName =  String(describing: dataArr[indexPath.row])
-        
+        self.hidesBottomBarWhenPushed = true
         navigationController?.pushViewController(twoAreaVC, animated: true)
-        
+        self.hidesBottomBarWhenPushed = false
     }
     
     override func didReceiveMemoryWarning() {
